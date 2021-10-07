0b60efe9363c0e8030453b60e882e823a12a129d
Breyten Ernsting <bje@dds.nl>
2021-10-07 14:48:57 +0200

    Add a button component.

---
+++ packages/button/Button.svelte
@@ -0,0 +1,21 @@
+<a {href}
+  this={component}
+  class="
+    oddk-button
+    {className}
+    {variant === 'raised' ? 'oddk-button-raised' : ''}
+    {variant === 'cta' ? 'oddk-button-cta' : ''}
+  "
+  {...exclude($$props, ['use', 'class', 'variant', ...dialogExcludes])}
+><slot></slot></a>
+
+<script>
+  import {exclude} from '@smui/common/exclude.js';
+
+  let dialogExcludes = [];
+  let className = '';
+  export {className as class};
+  export let variant = 'text';
+  export let href = null;
+  export let component;
+</script>
