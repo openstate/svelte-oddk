f99d7c2c88d3fa8db01892443478ff583c9a67b2
Breyten Ernsting <bje@dds.nl>
2021-10-08 16:46:06 +0200

    This event forwarder thing is working now.

--- packages/button/Button.svelte
+++ packages/button/Button.svelte
@@ -1,5 +1,5 @@
-<button {href}
-  this={component}
+<a {href}
+  use:forwardEvents
   class="
     oddk-button
     {className}
@@ -8,15 +8,19 @@
     {variant === 'cta' ? 'oddk-button-cta' : ''}
   "
   {...exclude($$props, ['use', 'class', 'variant', ...dialogExcludes])}
-><slot></slot></button>
+><slot></slot></a>
 
 <script>
-  import {exclude} from '@smui/common/exclude.js';
+  import {get_current_component} from 'svelte/internal';
+  import {forwardEventsBuilder} from '@soddk/common/forwardEvents.js';
+  import {exclude} from '@smui/common/exclude.js'; // TODO: uhm, need to factor this out`
+
+  const forwardEvents = forwardEventsBuilder(get_current_component());
 
   let dialogExcludes = [];
   let className = '';
+  export let use = [];
   export {className as class};
   export let variant = 'normal';
-  export let href = null;
-  export let component;
+  export let href = 'javascript:void(0);';
 </script>
