e657c0a890a0cbc56d5e34c232e5046b448debcd
Breyten Ernsting <bje@dds.nl>
2021-10-11 15:31:45 +0200

    Use event forwarder for profile stuff too.

--- packages/common/Profile.svelte
+++ packages/common/Profile.svelte
@@ -1,4 +1,6 @@
-<div class="oddk-profile oddk-profile-{type}" {...exclude($$props, ['use', 'class', 'type', 'byline'])}>
+<div
+  use:forwardEvents
+  class="oddk-profile oddk-profile-{type}" {...exclude($$props, ['use', 'class', 'type', 'byline'])}>
   <img src="/images/oddk/{avatar}" class="oddk-profile-avatar" />
   <div class="oddk-profile-details">
     <slot></slot>
@@ -9,7 +11,11 @@
 </div>
 
 <script>
+import {get_current_component} from 'svelte/internal';
+import {forwardEventsBuilder} from '@soddk/common/forwardEvents.js';
 import {exclude} from '@smui/common/exclude.js';
+
+const forwardEvents = forwardEventsBuilder(get_current_component());
 
 export let type = "person";
 export let avatar = type + ".svg";
