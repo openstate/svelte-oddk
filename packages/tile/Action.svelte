795f3e938bda37fff70333ef7c3a39520b5c2413
Breyten Ernsting <bje@dds.nl>
2021-10-14 14:37:20 +0200

    Separate class.

--- packages/tile/Action.svelte
+++ packages/tile/Action.svelte
@@ -1,9 +1,8 @@
-<svelte:component
-  this={component}
-  use={[forwardEvents]}
-  class="oddk-tile-action" {...exclude($$props, ['use', 'class', 'type'])}>
+<a {href}
+  use:forwardEvents
+  class="oddk-tile-action" {...exclude($$props, ['use', 'class'])}>
   <slot></slot>
-</svelte:component>
+</a>
 
 <script>
 import {get_current_component} from 'svelte/internal';
@@ -13,6 +12,5 @@ import A from '@soddk/common/A.svelte';
 import Button from '@soddk/button/';
 
 const forwardEvents = forwardEventsBuilder(get_current_component());
-export let href = null;
-export let component = href == null ? Button : A;
+export let href = 'javascript:void(0);'
 </script>
