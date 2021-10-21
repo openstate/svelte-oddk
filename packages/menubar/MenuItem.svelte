f926f45eaf967151215ead9653d713fed2ab3a05
Breyten Ernsting <bje@dds.nl>
2021-10-21 11:42:23 +0200

    Small fix.

--- packages/menubar/MenuItem.svelte
+++ packages/menubar/MenuItem.svelte
@@ -1,5 +1,6 @@
-<a {href} class="oddk-menubar-item" {alt}><slot></slot></a>
+<a {href} class="oddk-menubar-item" {alt} {use}><slot></slot></a>
 <script>
 export let href;
 export let alt;
+export let use;
 </script>
