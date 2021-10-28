6d494469ab4983d8bb40af8d12a7e9eb01bdbe34
Breyten Ernsting <bje@dds.nl>
2021-10-28 16:00:39 +0200

    Implement useActions for workspace and rename class.

--- packages/workspace/Workspace.svelte
+++ packages/workspace/Workspace.svelte
@@ -1,3 +1,18 @@
-<div class="soddk-workspace">
-<slot>You should put somethingg here.</slot>
+<div
+  use:useActions={use}
+  use:forwardEvents
+  class="oddk-workspace"
+  {...exclude($$props, ['use', 'class'])}>
+  <slot></slot>
 </div>
+
+<script>
+import {get_current_component} from 'svelte/internal';
+import {forwardEventsBuilder} from '@soddk/common/forwardEvents.js';
+import {exclude} from '@soddk/common/exclude.js';
+import {useActions} from '@soddk/common/useActions.js';
+
+const forwardEvents = forwardEventsBuilder(get_current_component());
+
+export let use = [];
+</script>
