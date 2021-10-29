704d763ce0e46cb5f534a521c36ab266f182a061
Breyten Ernsting <bje@dds.nl>
2021-10-29 15:58:23 +0200

    Add H2 with font spec.

---
+++ packages/common/H2.svelte
@@ -0,0 +1,20 @@
+<h2
+  use:useActions={use}
+  use:forwardEvents
+  class={h2_class}
+  {...exclude($$props, ['use', 'class'])}>
+  <slot></slot>
+</h2>
+
+<script>
+import {get_current_component} from 'svelte/internal';
+import {forwardEventsBuilder} from './forwardEvents.js';
+import {exclude} from './exclude.js';
+import {useActions} from './useActions.js';
+
+const forwardEvents = forwardEventsBuilder(get_current_component());
+
+export let use = [];
+export let variant = "normal";
+let h2_class = "oddk-h2-" + variant;
+</script>
--- packages/common/bare.js
+++ packages/common/bare.js
@@ -1,5 +1,6 @@
 import Profile from './Profile.svelte';
 import H1 from './H1.svelte';
+import H2 from './H2.svelte';
 
 export default Profile;
-export {H1};
+export {H1, H2};
