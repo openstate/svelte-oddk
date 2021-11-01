d0fb993695b588e51ca584938c52e52b9f4205f4
Breyten Ernsting <bje@dds.nl>
2021-11-01 17:19:36 +0100

    Add tag implementation.

---
+++ packages/common/Tag.svelte
@@ -0,0 +1,21 @@
+<a
+  use:useActions={use}
+  use:forwardEvents
+  {href}
+  class="oddk-tag oddk-tag-{variant}"
+  {...exclude($$props, ['use', 'href'])}>
+  <slot></slot>
+</a>
+
+<script>
+import {get_current_component} from 'svelte/internal';
+import {forwardEventsBuilder} from './forwardEvents.js';
+import {exclude} from './exclude.js';
+import {useActions} from './useActions.js';
+
+const forwardEvents = forwardEventsBuilder(get_current_component());
+
+export let use = [];
+export let href = 'javascript:void(0);';
+export let variant = 'normal';
+</script>
--- packages/common/bare.js
+++ packages/common/bare.js
@@ -1,6 +1,7 @@
 import Profile from './Profile.svelte';
 import H1 from './H1.svelte';
 import H2 from './H2.svelte';
+import Tag from './Tag.svelte';
 
 export default Profile;
-export {H1, H2};
+export {H1, H2, Tag};
