43050c426756a119bd1c9f3b256881073aceab49
Breyten Ernsting <bje@dds.nl>
2021-11-03 16:47:16 +0100

    Add an icon button.

---
+++ packages/icon-button/IconButton.svelte
@@ -0,0 +1,29 @@
+<a
+  use:useActions={use}
+  use:forwardEvents
+  {href}
+  class="
+    oddk-icon-button
+    {className}
+    {variant === 'normal' ? 'oddk-button-normal' : ''}
+    {variant === 'raised' ? 'oddk-button-raised' : ''}
+    {variant === 'cta' ? 'oddk-button-cta' : ''}
+  "
+  {...exclude($$props, ['href', 'use', 'class', 'variant', ...dialogExcludes])}
+><slot></slot></a>
+
+<script>
+  import {get_current_component} from 'svelte/internal';
+  import {forwardEventsBuilder} from '@soddk/common/forwardEvents.js';
+  import {exclude} from '@soddk/common/exclude.js';
+  import {useActions} from '@soddk/common/useActions.js';
+
+  const forwardEvents = forwardEventsBuilder(get_current_component());
+
+  let dialogExcludes = [];
+  let className = '';
+  export let use = [];
+  export {className as class};
+  export let variant = 'normal';
+  export let href = 'javascript:void(0);';
+</script>
---
+++ packages/icon-button/bare.js
@@ -0,0 +1,3 @@
+import IconButton from './IconButton.svelte';
+
+export default IconButton;
